96bac940ab7fa9771edcb303290678bc38f8fcc0
fabioxinxao <54850478+fabioxinxao@users.noreply.github.com>
2019-09-06 11:47:35 -0300

    Add files via upload

--- scanner.c
+++ scanner.c
@@ -3,71 +3,67 @@
 #include <string.h>
 #include <stdlib.h>
 
+//reserved words
+#define Rword_main 1
+#define Rword_if 2
+#define Rword_else 3
+#define Rword_do 4
+#define Rword_while 5
+#define Rword_for 6
+#define Rword_char 7
+#define Rword_int 8 
+#define Rword_float 9
+#define Rword_open_parent 10
+#define Rword_close_parent 11
+#define Rword_open_key 12
+#define Rword_close_key 13
+#define Rword_sum 14
+#define Rword_sub 15
+#define Rword_multi 16
+#define Rword_div 17
+#define Rword_assignment 18 //atribuicao
+#define Rword_equal 19 
+#define Rword_comma 20 //virgula
+#define Rword_point_virg 21 
+#define Rword_highest 22
+#define Rword_minor 23
+#define Rword_greater_equal 24 
+#define Rword_smaller_equal 25
+#define Rword_diferent 26
+#define TYPE_int 27
+#define TYPE_float 28
+#define TYPE_char 29
+#define ID 30 30
 
-#define Rword_main
-#define Rword_if
-#define Rword_else
-#define Rword_do
-#define Rword_while
-#define Rword_for
-#define Rword_char
-#define Rword_int
-#define Rword_float
-#define Rword_open_parent 
-#define Rword_close_parent 
-#define Rword_open_chave
-#define Rword_close_chave
-#define Rword_soma 
-#define Rword_sub 
-#define Rword_multi 
-#define Rword_div 
-#define Rword_atribuicao 
-#define Rword_igualdade 
-#define Rword_virgula 
-#define Rword_ponto_virg 
-#define Rword_maior 
-#define Rword_menor 
-#define Rword_maior_igual 
-#define Rword_menor_igual 
-#define Rword_diferente 
-#define TYPE_int 
-#define TYPE_float 
-#define TYPE_char
-#define ID 30
-
-typedef struct Token {
-	char lexema[300];
-	int tipo;
-}Struct_Token;
+typedef struct Token
+{
+	char lexem[200];
+	int type;
+}Str_Token;
 
 
-Struct_Token Scanner(FILE *arq);
+Str_Token scanner(FILE *arq);
 int RetornaTipoPalavra(char palavra[]);
 
 
-//VARIAVEIS GLOBAIS
-int linha = 1;
-int coluna = 1;
-Struct_Token lexm;
-
-
 //var global
-char caractere;
+int line = 1;
+int column = 1;
+Str_Token lexm;
+char character;
 
 
 //procedures
-int scanner(FILE *arq){
+int scanner(FILE *arq)
+{
 	int i = 0;
-	int linha = 0;
-	int coluna = 0;
-	
-	
-	
+	int line = 0;
+	int column = 0;
 }
 
-//main do program
+//main begin
 int main(int argv, char **argc){
-    int token;
+    Str_token token;
     FILE *arq;
     
 //abrir arquivo de texto para leitura
@@ -88,4 +84,21 @@ int main(int argv, char **argc){
 }
 
 
-
+Str_token scanner(FILE *arq) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
